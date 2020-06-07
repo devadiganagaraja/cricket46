a3155cb937d9498e24bf379d6f447e515bd30816
NAGARAJA DEVADIGA <nagaraja.devadiga@espn.com>
2020-06-07 18:07:48 +0530

    commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
